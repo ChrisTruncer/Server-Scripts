cad3c6973ae21c8aeda77d71cd32b01e9a18b4bf
Chris Truncer <CTruncer@christophertruncer.com>
2015-04-17 08:59:23 -0400

    Added fans script

---
+++ fans.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# From Hernandez while working on hash cracking box
+
+
+export DISPLAY=:0
+aticonfig --pplib-cmd "set fanspeed 0 100"
+
+export DISPLAY=:0.1
+aticonfig --pplib-cmd "set fanspeed 0 100"
+
+export DISPLAY=:0.2
+aticonfig --pplib-cmd "set fanspeed 0 100"
+
+export DISPLAY=:0.3
+aticonfig --pplib-cmd "set fanspeed 0 100"
